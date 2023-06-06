9bce0b8999968f5ff847e4d417929df6c92dcf5f
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-06 13:07:02 -0400

    wip: add navbar, topgames, searchbar, gamedetailpage routes

--- frontend/src/Pages/App/App.js
+++ frontend/src/Pages/App/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
+import NavBar from '../../Components/NavBar/NavBar';
+import TopGamesPage from '../TopGamesPage/TopGamesPage';
+import SearchBar from '../../Components/SearchBar/SearchBar';
+import GameDetailPage from '../GameDetailPage/GameDetailPage';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
-      <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/topgames" element={<TopGamesPage/>} />
+        <Route path="/search" element={<SearchBar/>} />
+        <Route path="/game/:name" element={<GameDetailPage/>} />
       </Routes>
     </div>
   );
