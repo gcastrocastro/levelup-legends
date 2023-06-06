7b07f1b8382657c2fc11a15774beee2516cdbd82
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-06 18:17:59 -0400

    wip: add TopGamesPage and api fetch funcationality

--- frontend/src/Pages/TopGamesPage/TopGamesPage.js
+++ frontend/src/Pages/TopGamesPage/TopGamesPage.js
@@ -1,5 +1,25 @@
+import {useState, useEffect} from 'react';
+import GameCard from '../../Components/GameCard/GameCard';
+import './TopGamesPage.css';
+
 export default function TopGamesPage() {
+    const [topGames, setTopGames] = useState([])
+
+    async function fetchTopGames() {
+        const response = await fetch(`https://rawg.io/api/games/lists/popular?discover=true&&page_size=20&page=1&key=c542e67aec3a4340908f9de9e86038af`);
+        const apiData = await response.json();
+        setTopGames(apiData.results);
+    }
+
+    useEffect(() => {
+        fetchTopGames()
+    },[]);
+
     return (
-        <h1>TopGamesPage</h1>
+        <div className='topgames-container'>
+            {topGames.map(game => {
+                return <GameCard game={game} key={game.name}/>
+            })}
+        </div>
     )
 }
