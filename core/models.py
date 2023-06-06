bda7fb60051e666d249761a48d1ac885954b6854
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-06 18:51:48 -0400

    wip: define userprofile model and migrate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    games = models.ManyToManyField('self', symmetrical=False)
+
+    def add_game(self, game_name):
+        game, _ = UserProfile.objects.get_or_create(user=self.user)
+        game.games.add(game_name)
+        return game_name
+
+    def remove_game(self, game_name):
+        game = self.games.filter(name=game_name).first()
+        if game:
+            self.games.remove(game)
+        return game
+
+    def get_games(self):
+        return self.games.all()
