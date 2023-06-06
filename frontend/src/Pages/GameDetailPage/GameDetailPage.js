05b125d21926c2ca5d3fb355754a6335ab842c4d
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-06 18:18:52 -0400

    wip: pass game object and show game images and title

--- frontend/src/Pages/GameDetailPage/GameDetailPage.js
+++ frontend/src/Pages/GameDetailPage/GameDetailPage.js
@@ -1,5 +1,24 @@
+import { useLocation } from 'react-router-dom';
+import './GameDetailPage.css';
+
 export default function GameDetailPage() {
+    const location = useLocation();
+    const game = location.state.game
+
+    console.log(game);
+
     return (
-        <h1>GameDetailPage</h1>
+        <div className="detailpage-container">
+            <div className="images">
+                <img className="header-image"src={game.background_image} alt={game.name}/>
+                <div className="screenshots">
+                    {game.short_screenshots.slice(1).map(ss => 
+                        <img src={`${ss.image}`}/>
+                    )}
+                </div>
+            </div>
+
+            <h2>{game.name}</h2>
+        </div>
     )
 }
