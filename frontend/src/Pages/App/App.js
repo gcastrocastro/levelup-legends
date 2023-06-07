7ebc7927c534ec5789e8cc88ea87bb4803653df2
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-07 15:22:35 -0400

    wip: add routes for register and login pages

--- frontend/src/Pages/App/App.js
+++ frontend/src/Pages/App/App.js
@@ -5,6 +5,8 @@ import NavBar from '../../Components/NavBar/NavBar';
 import TopGamesPage from '../TopGamesPage/TopGamesPage';
 import SearchBar from '../../Components/SearchBar/SearchBar';
 import GameDetailPage from '../GameDetailPage/GameDetailPage';
+import RegisterPage from '../RegisterPage/RegisterPage';
+import LoginPage from '../LoginPage/LoginPage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
         <Route path="/topgames" element={<TopGamesPage/>} />
         <Route path="/search" element={<SearchBar/>} />
         <Route path="/game/:name" element={<GameDetailPage/>} />
