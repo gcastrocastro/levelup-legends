1f17ba5938d224deb86241e80361af2ec5a15eb9
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-06 22:36:25 -0400

    wip: add register and csrf_cookie path

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import GetCSRFToken, SignUpView
+# , LoginView, LogoutView, CheckAuthenticatedView, DeleteAccountView
+
+urlpatterns = [
+    # path('authenticated', CheckAuthenticatedView.as_view()),
+    path('register', SignUpView.as_view()),
+    # path('login', LoginView.as_view()),
+    # path('logout', LogoutView.as_view()),
+    # path('delete', DeleteAccountView.as_view()),
+    path('csrf_cookie', GetCSRFToken.as_view())
+]
