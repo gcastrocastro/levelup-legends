47375c1de568781bb424bad6648987ac4c1f62ea
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-08 18:18:21 -0400

    wip: add infinite scroll to topgames page

--- frontend/src/Pages/TopGamesPage/TopGamesPage.js
+++ frontend/src/Pages/TopGamesPage/TopGamesPage.js
@@ -4,12 +4,41 @@ import './TopGamesPage.css';
 
 export default function TopGamesPage() {
     const [topGames, setTopGames] = useState([])
+    const [nextPageURL, setNextPageURL] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     async function fetchTopGames() {
         const response = await fetch(`https://rawg.io/api/games/lists/popular?discover=true&&page_size=20&page=1&key=c542e67aec3a4340908f9de9e86038af`);
         const apiData = await response.json();
+        console.log(apiData.next)
         setTopGames(apiData.results);
+        setNextPageURL(apiData.next);
     }
+
+    const loadMore = async () => {
+        if (nextPageURL && !isLoading) {
+            setIsLoading(true);
+            const response = await fetch(nextPageURL);
+            const apiData = await response.json();
+            setTopGames((prevResults) => [...prevResults, ...apiData.results]);
+            setNextPageURL(apiData.next);
+            setIsLoading(false);
+        }
+    };
+
+    const handleScroll = () => {
+        const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+        if (scrollTop + clientHeight >= scrollHeight - 20) {
+            loadMore();
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [nextPageURL, isLoading]);
 
     useEffect(() => {
         fetchTopGames()
@@ -20,6 +49,7 @@ export default function TopGamesPage() {
             {topGames.map(game => {
                 return <GameCard game={game} key={game.name}/>
             })}
+            {isLoading && <p>Loading...</p>}
         </div>
     )
 }
