3a7085d985115493e01be54c18b2d43457096c18
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-09 17:55:05 -0400

    wip: add game description and fix styling

--- frontend/src/Pages/GameDetailPage/GameDetailPage.js
+++ frontend/src/Pages/GameDetailPage/GameDetailPage.js
@@ -1,11 +1,24 @@
 import { useLocation } from 'react-router-dom';
 import './GameDetailPage.css';
 import Carousel from '../../Components/Carousel/Carousel';
+import { useState, useEffect } from 'react';
 
 export default function GameDetailPage() {
+    const [gameObject, setGameObject] = useState({});
     const location = useLocation();
     const game = location.state.game
-    console.log(game);
+
+    const getGameById = async () => {
+        const response = await fetch(`https://api.rawg.io/api/games/${game.id}?key=4dd0a3728ea6403a84545cc73b1ad93b`)
+        const data = await response.json()
+        console.log('data', data)
+        setGameObject(data)
+        console.log(gameObject)
+    }
+
+    useEffect(()=>{
+        getGameById()
+    }, []);
 
     return (
         <div className="detailpage-container">
@@ -14,6 +27,10 @@ export default function GameDetailPage() {
             </div>
             <h2>{game.name}</h2>
             <div className="game-details">
+                <div className="game-description">
+                    <h6>{gameObject.description_raw}</h6>
+                </div>
+                <div className="details">
                     <h3>Genres</h3>
                     <ul>
                         {game.genres.map(genre => (
@@ -21,11 +38,12 @@ export default function GameDetailPage() {
                         ))}
                     </ul>
                     <h3>Rating</h3>
-                    <h4>{game.rating}/5</h4>
+                    <h6>{game.rating}/5</h6>
                     <h3>Platforms</h3>
                     {game.platforms.map(platform => (
                             <h6>{platform.platform.name} ({platform.released_at}) </h6>
                         ))}
+                </div>
             </div>
         </div>
     )
