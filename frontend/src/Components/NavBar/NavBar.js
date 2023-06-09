daac033d9b96baf8615ecc8558a68b570a005f3f
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-09 14:05:16 -0400

    wip: add username to navbar

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,12 +1,15 @@
-import {NavLink} from 'react-router-dom';
+import {useNavigate, NavLink} from 'react-router-dom';
 import './NavBar.css';
 import { logout } from '../../actions/auth';
 import { connect } from 'react-redux';
 
-function NavBar({isAuthenticated, logout}) {
+function NavBar({isAuthenticated, logout, username}) {
+    const navigate = useNavigate();
+    
     const handleLogout = (e) => {
         e.preventDefault();
         logout();
+        navigate('/login');
     }
 
     return (
@@ -14,6 +17,7 @@ function NavBar({isAuthenticated, logout}) {
             <NavLink to="/"><img src="https://i.imgur.com/2jhQznM.png"/></NavLink>
             { isAuthenticated ? 
                 <>
+                    <h3> Welcome, {username} </h3>
                     <NavLink to="/topgames"> Top Games </NavLink>
                     <NavLink to="/search"> Search </NavLink>
                     <a onClick={handleLogout} href='#!'>Logout</a>
@@ -29,7 +33,8 @@ function NavBar({isAuthenticated, logout}) {
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+    isAuthenticated: state.auth.isAuthenticated,
+    username: state.profile.username
 });
 
 export default connect(mapStateToProps, { logout })(NavBar);
