2c11114b3c856dd9a32a7570f71e020ead9f8c07
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-08 23:20:49 -0400

    wip: add more game details and css

--- frontend/src/Pages/GameDetailPage/GameDetailPage.js
+++ frontend/src/Pages/GameDetailPage/GameDetailPage.js
@@ -5,7 +5,6 @@ import Carousel from '../../Components/Carousel/Carousel';
 export default function GameDetailPage() {
     const location = useLocation();
     const game = location.state.game
-
     console.log(game);
 
     return (
@@ -13,8 +12,29 @@ export default function GameDetailPage() {
             <div className="images">
                 <Carousel game={game}/>
             </div>
-
             <h2>{game.name}</h2>
+            <div className="video-details">
+                { game.clip && game.clip.clip ? (
+                <video autoPlay muted loop>
+                <source src={game.clip.clip} type="video/mp4" />
+                Your browser does not support the video tag.
+                </video>
+                ) : null} 
+                <div>
+                    <h3>Genres</h3>
+                    <ul>
+                        {game.genres.map(genre => (
+                            <li>{genre.name}</li>
+                        ))}
+                    </ul>
+                    <h3>Rating</h3>
+                    <h4>{game.rating}/5</h4>
+                    <h3>Platforms</h3>
+                    {game.platforms.map(platform => (
+                            <h6>{platform.platform.name} ({platform.released_at}) </h6>
+                        ))}
+                </div>
+            </div>
         </div>
     )
 }
