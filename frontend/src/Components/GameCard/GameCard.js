00d3e83633ba7199418f450e41a655c2a6c2e710
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-09 15:36:39 -0400

    wip: add videoclip to gamecard when hovered

--- frontend/src/Components/GameCard/GameCard.js
+++ frontend/src/Components/GameCard/GameCard.js
@@ -1,12 +1,33 @@
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
 import './GameCard.css';
 
 export default function GameCard({game}) {
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
     return (
-        <Link to={`/game/${game.name}`} state={{game: game}}>
+        <Link 
+        to={`/game/${game.name}`} 
+        state={{game: game}} 
+        onMouseEnter={handleMouseEnter} 
+        onMouseLeave={handleMouseLeave}>
             <div className="game-card">
-                <img src={game.background_image} alt={game.name}/>
-                <h2>{game.name}</h2>
+                {hovered && game.clip && game.clip.clip ? (
+                  <video autoPlay muted loop>
+                    <source src={game.clip.clip} type="video/mp4" />
+                    Sorry, your browser doesn't support embedded videos.
+                  </video>
+                ) : (
+                  <img src={game.background_image} alt={game.name} />
+                )}
+                <h3>{game.name}</h3>
             </div>
         </Link>
     )
